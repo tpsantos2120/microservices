bb27efce5896d58dc770182f4f55192b3e7a6263
Thiago Santos <thiagocroza472@gmail.com>
2022-05-01 23:02:54 -0300

    Change url path

--- fraud/src/main/java/com/projectx/fraud/controller/FraudCheckController.java
+++ fraud/src/main/java/com/projectx/fraud/controller/FraudCheckController.java
@@ -5,7 +5,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1/fraud")
+@RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
 public class FraudCheckController {
 
